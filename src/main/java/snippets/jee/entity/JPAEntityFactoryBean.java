c59250c58441cf823a91fe89defb2ebfe3e5c044
790013438 <790013438@qq.com>
2017-10-16 09:22:00 +0800

    entityManageFactory

---
+++ src/main/java/snippets/jee/entity/JPAEntityFactoryBean.java
@@ -0,0 +1,19 @@
+package snippets.jee.entity;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAEntityFactoryBean {
+
+    private EntityManagerFactory entityManagerFactory;
+
+    @PostConstruct
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("houseManagement");
+    }
+
+    public EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+}
