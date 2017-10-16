d4a494b4c9644ebcb8e44d0595b0e3435a726559
790013438 <790013438@qq.com>
2017-10-16 11:51:25 +0800

    单元测试，手动注入

---
+++ src/test/java/snippets/jee/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package snippets.jee.service;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserServiceTest {
+
+    @Test
+    public void testLoginAndRegister() {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("app.xml")) {
+            
+        }
+    }
+}
