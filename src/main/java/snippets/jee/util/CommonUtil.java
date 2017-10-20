eab020b1b9aa4cbf97da102efd24cd4b208f5f29
790013438 <790013438@qq.com>
2017-10-20 21:06:25 +0800

    生成图片

--- src/main/java/snippets/jee/util/CommonUtil.java
+++ src/main/java/snippets/jee/util/CommonUtil.java
@@ -1,10 +1,14 @@
 package snippets.jee.util;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public final class CommonUtil {
 
-    private static String string = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String CHARS = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int LEN = CHARS.length();
 
     private CommonUtil() {
     }
@@ -12,16 +16,31 @@ public final class CommonUtil {
     public static String generateCode (int len) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < len; ++i) {
-            stringBuilder.append(string.charAt((int)(string.length() * Math.random())));
+            int index = randomInt(0, LEN);
+            stringBuilder.append(CHARS.charAt(index));
         }
         return stringBuilder.toString();
     }
 
     public static BufferedImage generatedCodeImage (String code, int width, int height) {
-        return null;
+        BufferedImage codeImage = new BufferedImage(width, height, 1);
+        Graphics2D g = (Graphics2D) codeImage.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        g.setColor(Color.BLUE);
+        int size = (width - 10) / code.length();
+        g.setFont(new Font("Arial", Font.BOLD, size));
+        for (int i = 0; i < code.length(); ++i) {
+            String currentChar = code.substring(i, i + 1);
+            g.drawString(currentChar, 5 + size * i, height / 2);
+        }
+        return codeImage;
     }
 
-    public int randomInt (int start, int end) {
-        return (int) (start + Math.random() * end);
+    /**
+     * 生成指定范围的随机整数
+     */
+    public static int randomInt (int start, int end) {
+        return (int) (start + Math.random() * (end - start));
     }
 }
